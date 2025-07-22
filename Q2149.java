9221e68529b998c56895f27fc5458b72b7c08283
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-07-22 17:06:15 +0530

    seventh

---
+++ Q2149.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] result = new int[nums.length];
+        int posIndex = 0; 
+        int negIndex = 1; 
+
+        for (int num : nums) {
+            if (num >= 0) {
+                result[posIndex] = num;
+                posIndex += 2;
+            } else {
+                result[negIndex] = num;
+                negIndex += 2;
+            }
+        }
+
+        return result;
+    }
+}
+
+public class Q2149 {
+    
+}
--- Q53.java
+++ Q53.java
@@ -12,3 +12,4 @@ class Solution {
 public class Q53 {
     
 }
+
