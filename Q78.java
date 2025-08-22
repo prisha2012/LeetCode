dbf1460d16163527f9a05829044e1fa4409af8d3
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-08-22 18:57:59 +0530

    38.1

---
+++ Q78.java
@@ -0,0 +1,19 @@
+class Solution {
+      public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+    public void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> ans){
+        ans.add(new ArrayList<>(path));
+        for(int i=start;i<nums.length;i++){
+            path.add(nums[i]);
+            backtrack(nums,i+1,path,ans);
+            path.remove(path.size()-1);
+        }
+    }
+
+}
+public class Q78 {
+    
+}
