52e3be3bc1e1e4491c79eb0c34d6470cf3967622
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-09-11 19:18:58 +0530

    58

--- Q84.java
+++ Q84.java
@@ -4,18 +4,15 @@ class Solution {
          int maxArea = 0;
         int n = heights.length;
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             int h=(i==n?0:heights[i]);
-            while(!st.isEmpty()&& h<height[st.peek()]){
+            while(!st.isEmpty()&& h<heights[st.peek()]){
                 int height=heights[st.pop()];
                 int width=st.isEmpty()?i:i-st.peek()-1;
                 maxArea=Math.max(maxArea,height*width);
             }
-
+            st.push(i);
         }
         return maxArea;
     }
-}
-public class Q84 {
-    
 }
