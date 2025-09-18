c131508c87d6f7b1e304004a57f495757089743a
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-09-18 20:39:02 +0530

    65

--- Q1004.java
+++ Q1004.java
@@ -1,3 +1,21 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left=0,zerocount=0,maxlen=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]==0){
+                zerocount++;
+            }
+            while(zerocount>k){
+                if(nums[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }
+            maxlen=Math.max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+}
 public class Q1004 {
     
 }
