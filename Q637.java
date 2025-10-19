9ad54b0376674cc0b3e328c3a32f203fe46cefe5
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-10-19 22:28:38 +0530

    96

---
+++ Q637.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            res.add(sum / size);
+        }
+        return res;
+    }
+}
+
+public class Q637 {
+    
+}
