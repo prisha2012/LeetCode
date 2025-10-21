f0284d3d94506e697fac22243951efdfef0efade
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-10-21 19:44:11 +0530

    98

--- Q783.java
+++ Q783.java
@@ -1,3 +1,18 @@
+class Solution {
+    Integer prev = null, minDiff = Integer.MAX_VALUE;
+    public int minDiffInBST(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        if (prev != null) minDiff = Math.min(minDiff, node.val - prev);
+        prev = node.val;
+        inorder(node.right);
+    }
+}
+
 public class Q783 {
     
 }
